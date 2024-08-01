041a674ebdfa07390279bab90acc0078bba3ba38
Cjsah <cjsah@cjsah.net>
2024-08-01 15:51:26 +0800

    test plugin pass

--- TestPlugin/src/main/java/net/cjsah/bot/ext/TestPlugin.java
+++ TestPlugin/src/main/java/net/cjsah/bot/ext/TestPlugin.java
@@ -1,11 +1,19 @@
 package net.cjsah.bot.ext;
 
+import net.cjsah.bot.api.Api;
+import net.cjsah.bot.event.EventManager;
+import net.cjsah.bot.event.events.MessageEvent;
+import net.cjsah.bot.msg.MessageChain;
 import net.cjsah.bot.plugin.Plugin;
 
 public class TestPlugin extends Plugin {
     @Override
     public void onLoad() {
-        System.out.println("load");
+        EventManager.subscribe(MessageEvent.GroupMessageEvent.class, event -> {
+            if (event.getGroupId() == 799652476L && "/api".equals(event.getRawMessage())) {
+                Api.sendGroupMsg(event.getGroupId(), MessageChain.raw("插件测试"));
+            }
+        });
     }
 
     @Override
