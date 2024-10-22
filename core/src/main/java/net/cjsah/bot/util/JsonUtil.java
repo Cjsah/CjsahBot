1bcb340034d8631c7333111a1f7e187b7a581281
Cjsah <cjsah@cjsah.net>
2024-10-22 20:14:10 +0800

    jsonutil

--- core/src/main/java/net/cjsah/bot/util/JsonUtil.java
+++ core/src/main/java/net/cjsah/bot/util/JsonUtil.java
@@ -1,14 +1,10 @@
 package net.cjsah.bot.util;
 
-import cn.hutool.core.io.FileUtil;
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.JSONReader;
 
-import java.io.File;
 import java.io.Reader;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class JsonUtil {
@@ -29,35 +25,11 @@ public class JsonUtil {
         return JSON.parseObject(str, JSONReader.Feature.SupportSmartMatch);
     }
 
-    public static JSONObject deserializePath(String path) {
-        File file = new File(path);
-        String str = FileUtil.readString(file, StandardCharsets.UTF_8);
-        return JSON.parseObject(str, JSONReader.Feature.SupportSmartMatch);
-    }
-
     public static <T> T getObject(JSONObject json, String key, Class<T> clazz) {
         return json.getObject(key, clazz, JSONReader.Feature.SupportSmartMatch);
     }
 
     public static <T> List<T> getList(JSONObject json, String key, Class<T> clazz) {
         return json.getList(key, clazz, JSONReader.Feature.SupportSmartMatch);
-    }
-
-    public static <T> T convert(JSONObject json, Class<T> clazz) {
-        return json.to(clazz);
-    }
-
-    public static <T> T convert(Object json, Class<T> clazz) {
-        return convert((JSONObject) json, clazz);
-    }
-
-    public static <T> List<T> convertList(Object array, Class<T> clazz) {
-        return ((JSONArray) array).toList(clazz, JSONReader.Feature.SupportSmartMatch);
-    }
-
-    public static Object getOrDefault(JSONObject json, String key, Object defaultValue) {
-        Object value = json.get(key);
-        if (value == null) return defaultValue;
-        return value;
     }
 }
