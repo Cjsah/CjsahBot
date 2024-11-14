cbad0496cc148a627880b6f89c66e1a8fdd030f0
Cjsah <cjsah@cjsah.net>
2024-11-14 15:24:42 +0800

    移除无关命令和事件订阅

--- core/src/main/java/net/cjsah/bot/plugin/MainPlugin.java
+++ core/src/main/java/net/cjsah/bot/plugin/MainPlugin.java
@@ -53,103 +53,12 @@ public final class MainPlugin extends Plugin {
             CommandSource source = new CommandSource(event);
             CommandManager.execute(event.getCommandInfo(), source);
         });
-
-        EventManager.subscribe(CardButtonClickEvent.class, event -> {
-            log.info("room: {},channel: {}, user: {}, text: {}, value: {}",
-                    event.getRoomInfo().getName(),
-                    event.getChannelInfo().getName(),
-                    event.getSenderInfo().getNickname(),
-                    event.getButtonText(),
-                    event.getButtonValue()
-            );
-        });
     }
 
     @Command(value = "/botstop", permissions = HeyboxPermission.ADMIN)
     public static void botStop(CommandSource source) {
         source.sendFeedback("bot正在关闭...");
         Main.sendSignal(SignalType.STOP);
-    }
-
-
-    @Command(value = "/test", permissions = HeyboxPermission.ADMIN)
-    public static void test(CommandSource source) {
-        CommandEvent sender = source.sender();
-        String roomId = sender.getRoomInfo().getId();
-        String channelId = sender.getChannelInfo().getId();
-        CardBuilder builder1 = new CardBuilder(roomId, channelId)
-                .replay(sender.getMsgId())
-                .card(cart -> cart
-                        .section(section -> section
-                                .text(TextType.TEXT, "aaaa")
-                        )
-                        .section(section -> section
-                                .text(TextType.TEXT, "bbbb")
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                        )
-                        .section(section -> section
-                                .image("https://chat.max-c.com/pic/1844295655587745795.gif", Size.SMALL)
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                        )
-                        .section(section -> section
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                                .image("https://chat.max-c.com/pic/1844295655587745795.gif", Size.LARGE)
-                        )
-                        .section(section -> section
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                                .button("click", "unknown", false, Theme.DANGER)
-                        )
-                );
-        CardBuilder builder2 = new CardBuilder(roomId, channelId)
-                .replay(sender.getMsgId())
-                .card(card -> card
-                        .section(section -> section
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                                .image("https://chat.max-c.com/pic/1844295655587745795.gif", Size.LARGE)
-                                .endCheck()
-                        )
-                        .header(TextType.TEXT, "`aaaaa\n\n`一段文字` *内容*")
-                        .header(TextType.MARKDOWN, "aaaaa\n\n`一段文字` *内容*")
-                        .images("https://chat.max-c.com/pic/1844295655587745795.gif")
-                        .divider()
-                        .images("https://chat.max-c.com/pic/1844295655587745795.gif", "https://chat.max-c.com/pic/1844295655587745795.gif")
-                        .divider("分割线")
-                        .buttons(buttons -> buttons
-                                .button("click1", "unknown", false, Theme.DANGER)
-                                .button("click2", "https://server.cjsah.net:1002/", true, Theme.SUCCESS)
-                                .button("click3", "https://server.cjsah.net:1002/", false, Theme.PRIMARY)
-                        )
-                );
-        long time = System.currentTimeMillis() / 1000 + 2160000;
-        CardBuilder builder3 = new CardBuilder(roomId, channelId)
-                .replay(sender.getMsgId())
-                .card(card -> card
-                        .section(section -> section
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                                .image("https://chat.max-c.com/pic/1844295655587745795.gif", Size.LARGE)
-                        )
-                        .countdown(CountdownMode.DEFAULT, time)
-                )
-                .card(card -> card
-                        .section(section -> section
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                                .image("https://chat.max-c.com/pic/1844295655587745795.gif", Size.LARGE)
-                        )
-                        .countdown(CountdownMode.CALENDAR, time)
-                )
-                .card(card -> card
-                        .section(section -> section
-                                .text(TextType.MARKDOWN, "`111`**222**\n\n> 3333")
-                                .image("https://chat.max-c.com/pic/1844295655587745795.gif", Size.LARGE)
-                        )
-                        .countdown(CountdownMode.SECOND, time)
-                );
-
-        Api.sendCardMsg(builder1);
-        Api.sendCardMsg(builder2);
-        Api.sendCardMsg(builder3);
-
-
     }
 
 }
