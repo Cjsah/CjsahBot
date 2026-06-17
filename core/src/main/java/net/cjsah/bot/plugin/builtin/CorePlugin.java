75e5e8b2757a0b288b0f7b0af48c55cb1f77e66f
Cjsah <cjsah@cjsah.net>
2026-06-17 15:37:14 +0800

    update

--- core/src/main/java/net/cjsah/bot/plugin/builtin/CorePlugin.java
+++ core/src/main/java/net/cjsah/bot/plugin/builtin/CorePlugin.java
@@ -1,7 +1,6 @@
 package net.cjsah.bot.plugin.builtin;
 
 import com.google.gson.JsonNull;
-import com.mojang.datafixers.util.Either;
 import net.cjsah.bot.HeartBeatTimer;
 import net.cjsah.bot.MainApplication;
 import net.cjsah.bot.command.Commands;
@@ -17,19 +16,12 @@ import net.cjsah.bot.event.events.GroupMessageEvent;
 import net.cjsah.bot.event.events.HeartbeatEvent;
 import net.cjsah.bot.event.events.MessageEvent;
 import net.cjsah.bot.loader.DummyClassLoader;
-import net.cjsah.bot.packet.PacketHandler;
-import net.cjsah.bot.packet.request.payload.GetGroupMember;
-import net.cjsah.bot.packet.request.payload.GetGroupMembers;
-import net.cjsah.bot.packet.request.payload.RequestPacket;
-import net.cjsah.bot.packet.response.payload.GroupMember;
-import net.cjsah.bot.packet.response.payload.ResponsePacket;
 import net.cjsah.bot.plugin.Plugin;
 import net.cjsah.bot.plugin.PluginContainer;
 import net.cjsah.bot.plugin.PluginMetadata;
 import net.cjsah.bot.plugin.entry.BuiltinPluginEntryPointImpl;
 
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -78,19 +70,6 @@ public final class CorePlugin implements Plugin {
             GroupUserData sender = event.getSender();
             MainApplication.log.info("[群聊] [{}({})] [{}({})] => {}", event.getGroupName(), event.getGroupId(), sender.getCard(), sender.getUserId(), event.getMessage());
         });
-
-//        Commands.registerContext().register(context -> {
-//            return context.literal("test")
-//                .executes(c -> {
-//                    return 1;
-//                })
-//                .then(context.argument("arg1", AtArgument.atArg())
-//                    .executes(c -> {
-//                        Optional<Long> arg1 = AtArgument.get(c, "arg1");
-//                        return 1;
-//                    })
-//                );
-//        });
     }
 
     @SimpleCommand(value = "/botstop", permission = UserRole.ADMIN)
@@ -98,62 +77,4 @@ public final class CorePlugin implements Plugin {
         source.sendFeedback("bot正在关闭...");
         MainApplication.getInstance().halt();
     }
-
-    @SimpleCommand(value = "/test abc")
-    public static void test(CommandSource<?> source) {
-//        RequestPacket packet = new GetGroupMembers(756191906L);
-        RequestPacket packet = new GetGroupMember(756191906L, 2684117397L);
-        Either<GroupMember, String> res = PacketHandler.getInstance().send(packet);
-        Optional<String> error = res.right();
-        if (error.isPresent()) {
-            Commands.log.error("Failed to send feed back: {}", error.get());
-            return;
-        }
-        GroupMember member = res.left().get();
-
-
-        res.right().ifPresent(msg -> Commands.log.error("Failed to send feed back: {}", msg));
-    }
-
-    @SimpleCommand(value = "/jrrp")
-    public static void jrrp(CommandSource<?> source) {
-        long senderId = source.getSender().getUserId();
-        int rp = getJrrp(String.valueOf(senderId));
-        source.sendFeedback("您的今日人品为: %s".formatted(rp % 101));
-    }
-
-    @SimpleCommand(value = "/jrrp <qq:at>")
-    public static void jrrp(CommandSource<?> source, long qq) {
-        int rp = getJrrp(String.valueOf(qq));
-        source.sendFeedback("%s的今日人品为: %s".formatted("", rp % 101));
-    }
-
-    // 别人给的不知道哪里的算法
-
-    private static long rol(long num, int k) {
-        return (num << k) | (num >>> (64 - k));
-    }
-
-    private static long getHash(String str) {
-        long num = 5381;
-        for (int i = 0, len = str.length(); i < len; i++) {
-            num = rol(num, 5) ^ num ^ str.charAt(i);
-        }
-        return num ^ 0xA9956E6B53C2E4EFL;
-    }
-
-    private static int getJrrp(String userId) {
-        LocalDate now = LocalDate.now();
-        int dayOfYear = now.getDayOfYear();
-        int year = now.getYear();
-        int dayOfMonth = now.getDayOfMonth();
-
-        long hash1 = getHash("asdfgbn" + dayOfYear + "12#3$45" + year + "IUY");
-        long hash2 = getHash("QWERTY" + userId + "0*8&6" + dayOfMonth + "kjhg");
-
-        int num = (int) (Math.abs((hash1 / 3.0 + hash2 / 3.0) / 527.0) % 1001);
-        if (num >= 970) return 100;
-        return (int) Math.round(num / 969.0 * 99.0);
-    }
-
 }
